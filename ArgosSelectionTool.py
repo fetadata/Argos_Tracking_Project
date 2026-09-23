00418379bdb191330a3f783e835e4ffdbbc8e010
fetadata <js1213@duke.edu>
2026-09-23 00:03:50 -0400

    Initial commit of Argos Selection Script

---
+++ ArgosSelectionTool.py
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------
+# ArgosSelectionTool.py
+#
+# This tool reads in an Argos tracking data file
+# and allows t he user to identify the tracked sitings
+# found within a specified bounding box
+# Author: Josh Salzberg
+# Date: Fall 2026
+#-------------------------------------------------------------
